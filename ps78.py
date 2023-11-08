fc16d0f4899120c05d6e446f61e04e3e642a5258
pradeep <pradeepacharya626@gmail.com>
2023-11-08 10:54:56 +0530

    use of slider

--- ps78.py
+++ ps78.py
@@ -1,12 +1,6 @@
-                                                   #DAY 19
-#use of expanditure method
+                                        #DAY 19
+#exercise
+#use of slider
 
 import streamlit as st
-from PIL import Image
-
-with st.expander("Start camera") :
-    camera_img = st.camera_input("Camera")
-if camera_img :
-    img = Image.open(camera_img)
-    gray_img = img.convert("L")
-    st.image(gray_img)
+st.slider("Amount",min_value=10,max_value=30)
